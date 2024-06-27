5400922d48f295a8d31a2b44b159f54ead7276da
Kyle Belanger <kyleb44@hotmail.com>
2024-06-27 17:31:56 -0400

    update patient demo actions

--- app/logic/patient_info_logic.R
+++ app/logic/patient_info_logic.R
@@ -6,14 +6,27 @@ box::use(
   app/logic/database_actions[get_patient_data],
 )
 
-ID  <- "1c60a5da-1843-1978-d31a-ee5c060a43d2"
-columns  <- list("BIRTHDATE", "FIRST", "MIDDLE", "LAST", "RACE", "ETHNICITY")
-table  <- "patients"
+# ID  <- "1c60a5da-1843-1978-d31a-ee5c060a43d2"
+# columns  <- list("BIRTHDATE", "FIRST", "MIDDLE", "LAST", "GENDER",  "RACE", "ETHNICITY")
+# table  <- "patients"
 
-test_df  <- get_patient_data(columns, table, ID)
+# df  <- get_patient_data(columns, table, ID)
 
 
 #' @export
 pt_demo_table  <- function(df) {
+  df |>
+    dplyr::mutate(dplyr::across(BIRTHDATE, as.character)) |>
+    tidyr::pivot_longer(tidyselect::everything()) |>
+    purrr::pmap(function(name, value) {
+      shiny::tagList(
+        shiny::fluidRow(
+          shiny::h3(name),
+          shiny::p(value)
+        ),
+        shiny::hr()
+      )
+    })
 
 }
+
--- app/view/patient_info.R
+++ app/view/patient_info.R
@@ -4,23 +4,26 @@ box::use(
 
 box::use(
   app/logic/database_actions[get_patient_data],
+  app/logic/patient_info_logic[pt_demo_table],
 )
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
-  shiny::verbatimTextOutput(ns("value"))
   uiOutput(ns("pt_demo"))
-
 }
 
 #' @export
 server <- function(id, patient_id) {
   moduleServer(id, function(input, output, session) {
-    output$value <- shiny::renderPrint(patient_id())
-
     output$pt_demo  <- renderUI({
-      shiny::h3("It Worked")
+      columns  <- list("BIRTHDATE", "FIRST", "MIDDLE", "LAST", "GENDER",  "RACE", "ETHNICITY")
+      table  <- "patients"
+      ID  <- patient_id()
+
+      df  <- get_patient_data(columns,table,ID)
+
+      return(pt_demo_table(df))
     })
 
   })
