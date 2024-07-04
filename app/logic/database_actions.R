89c857615bebe2df5123a47419c65985f4ad5ada
Kyle Belanger <kyleb44@hotmail.com>
2024-07-04 08:35:29 -0400

    update data actions

--- app/logic/database_actions.R
+++ app/logic/database_actions.R
@@ -5,6 +5,8 @@ box::use(
   glue[glue_sql],
 )
 
+# should make function to open and close DB
+
 #' @export
 retrieve_data <- function(columns, table) {
   con  <- DBI$dbConnect(
@@ -30,3 +32,41 @@ get_patient_data  <- function(columns, table, ID) {
   return(data)
 }
 
+get_data  <- function(columns, table, ID, ID_col, joins = NULL) {
+  con  <- DBI$dbConnect(
+    RSQLite$SQLite(),
+    here("app", "synthea.sqlite")
+  )
+  joins <- joins
+  query <- glue_sql(
+    "SELECT {`columns`*} FROM {`table`}",
+    if(!is.null(joins)) joins,
+    "WHERE {ID_col} = {ID}",
+     .con = con,
+    .sep = " ")
+  data <- DBI$dbGetQuery(con, query)
+  DBI$dbDisconnect(con)
+  return(data)
+}
+
+#TESTING
+# ID <-  "4fe88ea1-1627-47d7-8dec-9d11f43faf0a" #for testing
+# columns <- list(
+#   DBI$Id("encounters", "START"),
+#   DBI$Id("encounters", "STOP"),
+#   DBI$Id("encounters", "PATIENT"), #this is the patient ID
+#   DBI$Id("encounters", "DESCRIPTION"),
+#   # DBI$Id("providers", "NAME"),
+#   glue_sql("`providers`.`NAME` AS Provider"),  #this works
+#   DBI$Id("organizations", "NAME")
+# )
+
+# table <- "encounters"
+
+# joins <- 
+#   "JOIN `providers` ON `encounters`.`PROVIDER` = `providers`.`Id`
+#   JOIN `organizations` ON `encounters`.`ORGANIZATION` = `organizations`.`Id`"
+
+# ID_col <- glue_sql("`encounters`.`PATIENT`")
+
+# get_data(columns, table, ID, ID_col, joins)
